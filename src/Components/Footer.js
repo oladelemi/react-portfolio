b590bab112753c35bed28a31c5352ed8ba403c18
oladelemi <lordthebash@gmail.com>
2020-09-08 00:16:01 -0700

    Updated react data

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,12 +19,12 @@ class Footer extends Component {
            </ul>
 
            <ul>
-            <li>Modified with love by the Clever Programmer team ♥️</li>
+            <li>Modded from Clever Programmer Github, check Readme for details ♥️</li>
            </ul>
 
            <ul className="copyright">
               <li>&copy; All credit goes to Tim Baker - Copyright 2017 Tim Baker</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>Author <a title="Lord Bash" href="https//oladelemi.com/">Lord Bash</a></li>
            </ul>
 
         </div>
